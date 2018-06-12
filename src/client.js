87bde3eee7b08e65b199abdb8c0387c87603ea61
Mel Gray <melgray@gmail.com>
2018-06-12 13:53:34 -0500

    Trying to make distinction between responsibilities of what should be functional vs what should contain state.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,5 @@
 var client_id = '5lch5g05xtkp5zhfva8g3u1xlxzwvb'
 
-
 /**
  * Dictionary for mapping query types to their respective paths on the API
  */
@@ -10,40 +9,119 @@ var routes = {
   }
 }
 
-function App() {
+/**
+ * App - Main 'App' Object
+ *
+ * @return {Object}  New 'App' with default state (empty result set)
+ */
+function App(dom) {
   this.state = { results: null }
+  if (typeof dom !== 'undefined') { this.dom = dom }
+  else { this.dom = document }
 }
 
+/**
+ * App.prototype.search - Perform a search on the API and render the result set in the API
+ *
+ * @param  {String} query A string describing what you're searching for
+ */
 App.prototype.search = function(query) {
-  var url    = UrlFor('search', {q: query, client_id: client_id, callback: 'window.jitters.fetched'})
+  var url    = url_for('search', {q: query, client_id: client_id, callback: 'window.jitters._fetched'})
   var skript = document.createElement('script')
   skript.src = url
   document.body.appendChild(skript)
 }
 
-App.prototype.fetched = function(data) {
+/**
+ * App.prototype.fetched - Used by the JSONP callback whenever a remote resource is loaded
+ *
+ * @param  {Object} data Results from the API. (https://dev.twitch.tv/docs/v5/reference/search/#search-streams)
+ */
+App.prototype._fetched = function(data) {
   console.log(data)
   this.state.results = data
   if (this.state.results) {
-    updateUI(this.state.results)
+    this._updateUI(this.state.results)
   }
 }
 
-var updateUI = function(results) {
-  document.getElementById('results-controls').innerHTML = `<p>Total ${results._total}</p>`
-  document.getElementById('results').innerHTML = results.streams.map(buildResult).join('')
+
+/**
+ * var page_count - Calculates the number of pages in a result set
+ *
+ * @param  {Integer} results  Integer representing total number of results
+ * @param  {Integer} per_page Integer representing total number of results per page
+ * @return {Integer}          Integer representing total number of pages
+ */
+var page_count = function(results, per_page) {
+  if (!per_page) { per_page = 10 }
+  var result = results/per_page
+  if (results % per_page) { return Math.floor(result)+1 }
+  else { return result }
 }
 
 /**
- * UrlFor - Returns a url for a particular query type & params
+ * _url_for - Returns a url for a particular query type & params
  *
  * @param  {String} query_type Type of query you are interested in making
  * @param  {Object} params     Arguments for the url endpoint
  * @return {String}            URL for endpoint
  */
-function UrlFor(query_type, params) {
+var url_for = function(query_type, params) {
   var builder = new UrlBuilder(query_type, params)
   return builder.build()
+}
+
+/**
+ * image_url - Builds an image url using the template url in a stream object
+ *
+ * @param  {Object} stream Stream object from results set.
+ * @return {String}        URL to use as an image thumbnail for a string
+ */
+var image_url = function(stream) {
+  if (stream.preview.template) {
+    var result  = stream.preview.template.replace("{width}", 125)
+    result      = result.replace("{height}", 125)
+    return result
+  }
+  return "http://via.placeholder.com/125x125"
+}
+
+
+/**
+ * _build_result - This will build an html slug for use in populating a result set
+ *
+ * @param  {Object} stream Object representing a stream.
+ * @return {String}        HTML partial with populated fields
+ */
+var _build_result = function(stream) {
+  var template =
+  `<div class="result">
+    <img class='stream-img' src="${image_url(stream)}"/><h1>${stream.game}</h1>
+    <h2>${stream.game} - ${stream.viewers} Viewers</h2>
+    <p>${stream.channel.status}</p>
+  </div>`
+  return template
+}
+
+/**
+ * App.prototype._updateUI - Called when the app should update it user interface
+ *
+ */
+App.prototype._updateUI = function(results) {
+  addTotalCount(results)
+  addPagingControls(results)
+  document.getElementById('results').innerHTML = results.streams.map(_build_result).join('')
+}
+
+var addTotalCount = function(results) {
+  document.getElementById('results-controls').innerHTML = `<p>Total ${results._total}</p>`
+}
+
+var addPagingControls = function(results) {
+  var node        = document.createElement("span")
+  node.innerHTML  = `<span>&#8678; hi &#8680;</span>`
+  document.getElementById('results-controls').appendChild(node)
 }
 
 /**
@@ -87,34 +165,15 @@ UrlBuilder.prototype.build = function() {
   return url
 }
 
-function Search(query) {
-  this.method = 'GET'
-  this.url    = UrlFor('search', {q: query})
-  return this
-}
-
-var buildResult = function(result) {
-  var template =
-  `<div class="result">
-    <img class='stream-img' src="${buildImageURL(result.preview.template, 125, 125)}"/><h1>${result.game}</h1>
-    <h2>${result.game} - ${result.viewers} Viewers</h2>
-    <p>hi</p>
-  </div>`
-  return template
-}
-
-var buildImageURL = function(previewTemplateURL, width, height) {
-  var result  = previewTemplateURL.replace("{width}", width)
-  result      = result.replace("{height}", height)
-  return result
-}
-
+/// Make available to the test harness
 module.exports = {
-  Search: Search,
-  UrlFor: UrlFor,
-  buildImageURL: buildImageURL
+  App: App,
+  url_for: url_for,
+  image_url: image_url,
+  page_count: page_count
 }
 
+/// Make available to the browser window for initialization
 if (typeof window !== 'undefined') {
   window.App = App
 }
--- test/search-test.js
+++ test/search-test.js
@@ -1,20 +1,37 @@
 var test = require('tape')
 var client = require('../src/client')
 
+var app = new client.App({})
+var stream = {
+  preview: {
+    template: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_rotterdam08-{width}x{height}.jpg'
+  }
+}
+
+var results = {
+
+}
+
 test('that we can build a url', function(t) {
   t.plan(2)
-  t.equals(client.UrlFor('search', {q: 'starcraft'}),
+  t.equals(client.url_for('search', {q: 'starcraft'}),
           'https://api.twitch.tv/kraken/search/streams?q=starcraft',
           'url #1 should be correct')
 
-  t.equals(client.UrlFor('search', {q: 'rocket league'}),
+  t.equals(client.url_for('search', {q: 'rocket league'}),
           'https://api.twitch.tv/kraken/search/streams?q=rocket%20league',
           'url #2 should be correct')
 })
 
 test('that we can build an image url using a template', function(t){
   t.plan(1)
-
-  var url = client.buildImageURL('https://static-cdn.jtvnw.net/previews-ttv/live_user_rotterdam08-{width}x{height}.jpg', 125, 125)
+  var url = client.image_url(stream)
   t.equals(url, 'https://static-cdn.jtvnw.net/previews-ttv/live_user_rotterdam08-125x125.jpg', 'image url should template properly')
+})
+
+test('that we can calulate the number of pages in a result set', function(t){
+  t.plan(3)
+  t.equals(client.page_count(138), 14, "138 results should be 14 pages")
+  t.equals(client.page_count(100), 10, "100 results should be 10 pages")
+  t.equals(client.page_count(122), 13, "122 results should be 13 pages")
 })
